40792de42681cd4f98417afa9a132027789c795e
Duong <pduong@deloitte.com>
2019-04-08 12:14:53 -0400

    Trying to test Signature Pad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConstructingComponent } from './constructing/constructing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConstructingComponent
   ],
   imports: [
     BrowserModule,
