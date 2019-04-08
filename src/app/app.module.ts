d1e114afeca818d0aa92f41c1d0c727bb8bf8d28
Duong <pduong@deloitte.com>
2019-04-08 13:45:55 -0400

    trying implement a dataview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConstructingComponent } from './constructing/constructing.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConstructingComponent,
-    DashboardComponent
+    DashboardComponent,
+    MyNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SignaturePadModule
+    SignaturePadModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MyNavComponent } from '../my-nav/my-nav.component';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
