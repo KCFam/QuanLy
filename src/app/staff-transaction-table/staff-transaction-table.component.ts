c41cce4c8e23c1bf96b8053a86f3d5fdc7744299
Duong <pduong@deloitte.com>
2019-04-09 18:15:57 -0400

    Applied model

--- src/app/staff-transaction-table/staff-transaction-table.component.ts
+++ src/app/staff-transaction-table/staff-transaction-table.component.ts
@@ -12,52 +12,7 @@ import {
   MatDialogConfig
 } from '@angular/material/dialog';
 
-export interface DialogData {
-  animal: string;
-  name: string;
-}
-@Component({
-  selector: 'app-staff-transaction-dialog',
-  templateUrl: 'staff-transaction-dialog.module.html',
-  styleUrls: ['staff-transaction-dialog.module.scss'],
-})
-export class StaffTransactionDialogComponent {
-
-  userData: UserData;
-
-  constructor(
-    private dialogRef: MatDialogRef<StaffTransactionDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) data) {
-
-    this.userData = data;
-  }
-
-  ngOnInit() {
-  }
-
-  save() {
-    this.dialogRef.close("fdafA");
-  }
-
-  close() {
-    this.dialogRef.close();
-  }
-
-}
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
-
-/** Constants used to fill up our data base. */
-const COLORS: string[] = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+import { StaffTransactionService, StaffTransactionModel } from '../staff-transaction.service';
 
 @Component({
   selector: 'app-staff-transaction-table',
@@ -65,18 +20,15 @@ const NAMES: string[] = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
   styleUrls: ['./staff-transaction-table.component.scss']
 })
 export class StaffTransactionTableComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'color', 'action'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns: string[] = ['Ten Tho', 'Khuon', 'Loai Toc', 'So Day', 'Tien Cong', 'Tong Tien', 'action'];
+  dataSource: MatTableDataSource<StaffTransactionModel>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private dialog: MatDialog) {
-    // Create 100 users
-    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
-
+  constructor(private dialog: MatDialog, private staffTransactionService: StaffTransactionService) {
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource( staffTransactionService.getStaffTransactions() );
   }
 
   ngOnInit() {
@@ -92,14 +44,14 @@ export class StaffTransactionTableComponent implements OnInit {
     }
   }
 
-  openDialog( userData: UserData) {
+  openDialog( staffTransaction : StaffTransactionModel) {
 
     const dialogConfig = new MatDialogConfig();
 
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
 
-    dialogConfig.data = userData;
+    dialogConfig.data = staffTransaction;
 
     const dialogRef = this.dialog.open(StaffTransactionDialogComponent, dialogConfig);
     dialogRef.afterClosed().subscribe(
@@ -109,15 +61,30 @@ export class StaffTransactionTableComponent implements OnInit {
 
 }
 
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+@Component({
+  selector: 'app-staff-transaction-dialog',
+  templateUrl: 'staff-transaction-dialog.module.html',
+  styleUrls: ['staff-transaction-dialog.module.scss'],
+})
+export class StaffTransactionDialogComponent {
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
+  staffTransaction: StaffTransactionModel;
+
+  constructor(
+    private dialogRef: MatDialogRef<StaffTransactionDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
+
+    this.staffTransaction = data;
+  }
+
+  ngOnInit() {
+  }
+
+  save() {
+    this.dialogRef.close("fdafA");
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 }
--- src/app/staff-transaction.service.ts
+++ src/app/staff-transaction.service.ts
@@ -17,15 +17,21 @@ export interface StaffTransactionModel {
   providedIn: 'root'
 })
 export class StaffTransactionService {
+  staffTransactions = ELEMENT_DATA;
 
   constructor() { }
+
+  public getStaffTransactions() : StaffTransactionModel[] {
+    return this.staffTransactions;
+  }
+
 }
 
 // CREATE TEST DATA
 const ELEMENT_DATA: StaffTransactionModel[] = [
   { receivedDate: new Date(), returnedDate: null, staffID: "1", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
-  { receivedDate: new Date(), returnedDate: null, staffID: "1", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
-  { receivedDate: new Date(), returnedDate: null, staffID: "1", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
-  { receivedDate: new Date(), returnedDate: null, staffID: "1", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
-  { receivedDate: new Date(), returnedDate: null, staffID: "1", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"}
+  { receivedDate: new Date(), returnedDate: null, staffID: "5", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
+  { receivedDate: new Date(), returnedDate: null, staffID: "7", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
+  { receivedDate: new Date(), returnedDate: null, staffID: "12", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"},
+  { receivedDate: new Date(), returnedDate: null, staffID: "18", receivedItem: "item1", numberLinesReturned: 20, salary: 30.4, note: "", total: 20, signature: "dafda", lashType: "Type1"}
 ];
