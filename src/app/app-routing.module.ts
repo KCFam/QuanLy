3a868077cd5b088efd30225a52ffaf82036a667b
Paul Duong <dongphuong88@gmail.com>
2019-04-10 00:32:16 -0400

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ConstructingComponent } from './constructing/constructing.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: DashboardComponent
-  },
-  {
-    path: "Constructing",
-    component: ConstructingComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,18 @@
-// SYSTEM
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-// ANGULAR MATERIAL 2
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@angular/cdk/layout';
-import { 
-  MatToolbarModule, 
-  MatButtonModule, 
-  MatSidenavModule, 
-  MatIconModule, 
-  MatListModule,
-  MatFormFieldModule,
-  MatPaginatorModule,
-  MatTableModule,
-  MatInputModule
-} from '@angular/material';
-import {
-  MatDialogModule 
-} from '@angular/material/dialog';
-
-// 3RD PARTY
-import { SignaturePadModule } from 'angular2-signaturepad';
-
-// PROJECT
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ConstructingComponent } from './constructing/constructing.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { StaffTransactionTableComponent, StaffTransactionDialogComponent } from './staff-transaction-table/staff-transaction-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ConstructingComponent,
-    DashboardComponent,
-    StaffTransactionTableComponent,
-    StaffTransactionDialogComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    SignaturePadModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatFormFieldModule,
-    MatPaginatorModule,
-    MatTableModule,
-    MatInputModule,
-    FormsModule,
-    MatDialogModule
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [StaffTransactionDialogComponent,StaffTransactionTableComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
